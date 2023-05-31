bcb3fbe3454f5b84822ed1ad82c8c5ac513e3cfd
Melissa Dufrechou <dufrechou82@gmail.com>
2023-05-31 17:01:38 -0500

    Pulls measurements and ingredients at zero index

--- index.js
+++ index.js
@@ -1,27 +1,76 @@
+//Selects document elements for DOM manipulation
+
+let ingredients = document.getElementById("ingredients")
+let measurements = document.getElementById("measurements")
+let image = document.getElementById("image")
+let instructions = document.getElementById("instructions")
+let drinkName = document.getElementById("drink-name")
+
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 document.querySelector("#next").addEventListener("click", goForward);
 document.querySelector("#back").addEventListener("click", goBack);
 document.querySelector("button").addEventListener("click", getDrink);
 
-// let counter = 0;
-
-function getDrink() {
-  let drink = document.querySelector("input").value;
-
-  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
-    .then((res) => res.json()) // parse response as JSON
-    .then((data) => {
-      console.log(data.drinks);
-      counter = 0;
-
-      document.querySelector("h2").innerText = data.drinks[0].strDrink;
-      document.querySelector("img").src = data.drinks[0].strDrinkThumb;
-      document.querySelector("h3").innerText = data.drinks[0].strInstructions;
-    })
-    .catch((err) => {
-      console.log(`error ${err}`);
-    });
+async function getDrink(){
+  const drink = document.querySelector("input").value
+  const response = await fetch(
+    `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`
+  );
+  const data = await response.json();
+  counter = 0;
+  render(data)
+  getIngredients(data)
+  getMeasurements(data)
+  console.log(data.drinks[0])
 }
+
+function render(data){
+  instructions.innerHTML = data.drinks[0].strInstructions;
+  drinkName.innerHTML = data.drinks[0].strDrink;
+  image.src = data.drinks[0].strDrinkThumb;
+}
+
+const items = [];
+
+function getIngredients(data){
+    for (let i = 0; i<=15; i++){
+      let ingredient = data.drinks[0][`strIngredient${i}`];
+      console.log(ingredient)
+      if(ingredient){
+        items.push(ingredient)
+      }
+    }
+    items.forEach((item) => {
+      let li = document.createElement("li");
+      li.innerText = item;
+      ingredients.appendChild(li);
+    })
+    
+  
+
+  console.log(items)
+}
+
+const measure = [];
+
+function getMeasurements(data) {
+  for (let i = 0; i <= 15; i++) {
+    let measurement = data.drinks[0][`strMeasure${i}`];
+    console.log(measurement);
+    if (measurement) {
+      measure.push(measurement);
+    }
+  }
+      measure.forEach((item) => {
+        let li = document.createElement("li");
+        li.innerText = `${item} - `;
+        measurements.appendChild(li);
+      });
+
+
+  console.log(measure);
+}
+
 
 function goForward() {
   let drink = document.querySelector("input").value;
@@ -35,9 +84,9 @@ function goForward() {
       }
 
       counter++;
-      document.querySelector("h2").innerText = data.drinks[counter].strDrink;
+      document.getElementById("instructions").innerText = data.drinks[counter].strDrink;
       document.querySelector("img").src = data.drinks[counter].strDrinkThumb;
-      document.querySelector("h3").innerText =
+      document.getElementById("instructions").innerText =
         data.drinks[counter].strInstructions;
 
       // count();
@@ -59,9 +108,10 @@ function goBack() {
         counter = data.drinks.length;
       }
       counter--;
-      document.querySelector("h2").innerText = data.drinks[counter].strDrink;
+      document.getElementById("instructions").innerText =
+        data.drinks[counter].strDrink;
       document.querySelector("img").src = data.drinks[counter].strDrinkThumb;
-      document.querySelector("h3").innerText =
+      document.getElementById("instructions").innerText =
         data.drinks[counter].strInstructions;
     })
     .catch((err) => {
@@ -69,19 +119,3 @@ function goBack() {
     });
 }
 
-// function count(){
-//   let drink = document.querySelector('input').value
-
-//   fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
-//   .then(res => res.json()) // parse response as JSON
-//   .then(data => {
-//     console.log(data.drinks)
-//     for(let counter = 0 ; counter < data.drinks.length; counter++) {
-//       clearInterval(interval);
-//       return;
-//     }
-//   })
-//   .catch(err => {
-//     console.log(`error ${err}`)
-//   });
-// }
